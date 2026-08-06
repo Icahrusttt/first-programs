fe7ce68a4248af529bfa85c7fb7db2381b8f06f5
Pesto <adisatrotman77@gmail.com>
2026-08-06 17:14:12 -0400

    vowel_count.py complete

--- vowel_count.py
+++ vowel_count.py
@@ -1,1 +1,20 @@
 #vowel_count.py
+word = str("Programming is something I'm relearning so I must be paitient with myself")
+v_count = 0
+c_count = 0
+vowels = ['a','e','i','o','u','y']
+
+for letter in word:
+    #print(letter)
+    #print("----------")
+    for vowel in vowels:
+        #print(f"-{vowel}-")
+        if letter == vowel:
+            #print(f"{vowel} = {letter}")
+            print(letter)
+            v_count += 1
+        else:
+            #print(letter)
+            c_count += 1
+print(f"Vowel Count: {v_count}")
+print(f"Constanant Count: {c_count}")
